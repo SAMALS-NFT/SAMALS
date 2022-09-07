e03c68abb4dff484eae8c831f91da25b290f72d7
권성호 <maystar8956@naver.com>
2022-09-07 15:30:00 +0900

    feat : 웹페이지 기본세팅 (#5)

--- Client/src/pages/Create.js
+++ Client/src/pages/Create.js
@@ -8,17 +8,17 @@ import Image from "../components/base/Image";
 import Select from "../components/base/Select";
 import TextInput from "../components/base/TextInput";
 import { Colors } from "../constants/Colors";
-import {AiOutlineSearch} from 'react-icons/ai';
+import { AiOutlineSearch } from "react-icons/ai";
 import Header from "../components/Header";
 import { useEthers, useEtherBalance } from "@usedapp/core";
 
 const Create = () => {
-  const {activateBrowserWallet, account} = useEthers();
+  const { activateBrowserWallet, account } = useEthers();
   const etherBalance = useEtherBalance(account);
 
-  useEffect(()=>{
+  useEffect(() => {
     activateBrowserWallet();
-  },[])
+  }, []);
 
   return (
     <>
